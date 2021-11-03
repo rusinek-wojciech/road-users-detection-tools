2b39a51e3a987915d965bda93238c75afb60e65a
iKinsure <wrotekpl@gmail.com>
2021-11-03 13:46:49 +0100

    resolved merge conflict

--- xml_resize.py
+++ xml_resize.py
@@ -3,30 +3,7 @@ import glob
 import xml.etree.ElementTree as ET
 
 
-<<<<<<< Updated upstream
-# changing xml parameters
-def xml(path, scale):
-    for xml_file in glob.glob(path + '/*.xml'):
-        print(xml_file)
-        tree = ET.parse(xml_file)
-        root = tree.getroot()
-        size = root.find('size')
-        object = root.find('object')
-        bndbox = object.find('bndbox')
-        xmin = bndbox.find('xmin')
-        ymin = bndbox.find('ymin')
-        xmax = bndbox.find('xmax')
-        ymax = bndbox.find('ymax')
-        width = size.find('width')
-        height = size.find('height')
-        xmin.text = str(int(xmin.text) * scale)
-        ymin.text = str(int(ymin.text) * scale)
-        xmax.text = str(int(xmax.text) * scale)
-        ymax.text = str(int(ymax.text) * scale)
-        width.text = str(int(width.text) * scale)
-        height.text = str(int(height.text) * scale)
-        tree.write(xml_file)
-=======
+# changing xml file parameters
 def formal_xml(xml_file, scale):
     print(xml_file)
     tree = ET.parse(xml_file)
@@ -47,7 +24,6 @@ def formal_xml(xml_file, scale):
     width.text = str(int(int(width.text) * scale))
     height.text = str(int(int(height.text) * scale))
     tree.write(xml_file)
->>>>>>> Stashed changes
 
 
 def main():
